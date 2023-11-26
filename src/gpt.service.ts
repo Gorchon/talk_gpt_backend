6d9e7e7873969de5d0e657da9aeb45eb6a6ef5fa
José María Soto valenzuela <116988804+Gorchon@users.noreply.github.com>
2023-11-26 03:23:31 -0600

    Added a second database with my hearth rate

--- src/gpt.service.ts
+++ src/gpt.service.ts
@@ -15,16 +15,23 @@ export class ChatService {
       apiKey: process.env.OPENAI_API_KEY,
     });
 
-    // Initialize Firebase Admin SDK
-    const serviceAccount = require('/Users/chema./Downloads/programming/talk-gpt-backend/firebaseKey.json');
+    // Initialize Firebase Admin SDK for emotional data
+    const emotionsServiceAccount = require('/Users/chema./Downloads/programming/talk-gpt-backend/firebaseKey.json');
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(emotionsServiceAccount),
       databaseURL: 'https://iot-project-cows-default-rtdb.firebaseio.com/',
     });
+
+    // Initialize Firebase Admin SDK for cardiac data
+    const cardiacServiceAccount = require('/Users/chema./Downloads/programming/talk-gpt-backend/bitsKey.json');
+    const cardiacApp = admin.initializeApp({
+      credential: admin.credential.cert(cardiacServiceAccount),
+      databaseURL: 'https://car-control-9599f-default-rtdb.firebaseio.com/',
+    }, 'cardiacRhythm'); // Use an alias to distinguish between databases
   }
 
   async getLatestEmotionAndName(): Promise<{ emotion: string | null; name: string | null }> {
-    // Get the most recent entry from the emotions collection
+    // Use the 'emotions' database instance
     const emotionsSnapshot = await admin
       .database()
       .ref('emotions')
@@ -34,20 +41,34 @@ export class ChatService {
 
     const emotions = emotionsSnapshot.val();
     const emotionData = emotions ? emotions[Object.keys(emotions)[0]].emotion : null;
-
-    // Assuming there is a 'name' field in your Firebase data
     const nameData = emotions ? emotions[Object.keys(emotions)[0]].name : null;
 
     return { emotion: emotionData, name: nameData };
   }
 
+  async getLatestCardiacData(): Promise<{ bpm: number | null }> {
+    // Use the 'cardiacRhythm' database instance directly
+    const cardiacSnapshot = await admin
+      .app('cardiacRhythm')
+      .database()
+      .ref('BPM')
+      .orderByKey()
+      .limitToLast(1)
+      .once('value');
+
+    const bpmData = cardiacSnapshot.val();
+    // Extract relevant data and return it
+    return { bpm: bpmData };
+  }
+
   async chatWithGPT(content: string): Promise<string> {
     const { emotion, name } = await this.getLatestEmotionAndName();
+    const { bpm } = await this.getLatestCardiacData();
 
     // Add user's input, emotional context, and name to the conversation history
     this.conversationHistory.push({
       role: 'user',
-      content: `${content} I am feeling ${emotion} my name is ${name}.`,
+      content: `${content} I am feeling ${emotion} my name is ${name} and my heart rate is ${bpm} Beats per minute.`,
     });
 
     const chatCompletion = await this.openai.chat.completions.create({
